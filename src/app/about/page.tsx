e7b479bc195396926dab81908dcbe96f55acd22c
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-12-10 01:56:01 +0600

    added about write

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main className='pt-[100px] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 w-full'>About</main>
+    <main className='pt-[100px] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 w-full'>This is a About page</main>
   )
 }
 
